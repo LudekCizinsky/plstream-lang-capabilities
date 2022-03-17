a42cd53ce87696e1c6819d23bb30b7549cc5103d
LudekCizinsky <ludekcizinsky11@gmail.com>
2022-03-17 23:48:02 +0100

    Changed folder structure so it is more managable for future phases.

--- src/scripts/baseline/_majority_class.py
+++ src/scripts/baseline/_majority_class.py
@@ -1,22 +1,21 @@
-# _majority_class.py
 import numpy as np
-from tensorboard import summary
 
-class MajorityClass():
-  def __init__(self):
-    """the majority class classifier"""
+class MajorityClass:
+
+  def __init__(self): 
     self._majority = None
     self._probs = None
 
   def fit(self, X, y):
-    identifier, counts = np.unique(np.array(y), return_counts=True) # Counting the occurances of 0 and 1
+    identifier, counts = np.unique(np.array(y), return_counts=True)
     self._probs = counts/len(y)
     self._majority = identifier[np.argmax(counts)]
 
   def predict(self, X):
-    return np.empty(len(X)).fill(self._majority)
+    yhat = np.empty(len(X))
+    yhat.fill(self._majority)
+    return yhat
 
   def predict_probability(self, X):
     return np.array([self._probs for _ in range(len(X))])
-
 
