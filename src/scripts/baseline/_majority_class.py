b9f2c85625c89b0e501188f2ed80728843905438
IbenH <iben@midsund.dk>
2022-03-17 15:05:42 +0100

    Update _majority_class.py

--- src/scripts/baseline/_majority_class.py
+++ src/scripts/baseline/_majority_class.py
@@ -3,15 +3,15 @@ import numpy as np
 from tensorboard import summary
 
 class MajorityClass():
-  def __init__(self, matrix):
+  def __init__(self, Y):
     """the majority class classifier"""
-    self.matrix = matrix
+    self.Y = Y
     return
 
-  def majority(self, matrix):
-    sentiment = matrix[-1]                                        # Acess the column with the sentiment 
-    identifier, counts = np.unique(sentiment, return_counts=True) # Counting the occurances of 0 and 1
-    count_sent = dict(zip(identifier, counts))                    # Zip the counts into a dictionary
-    find_majority = max(count_sent, key=count_sent.get)           # Find the majority count from the dict
+  def majority(self, Y):
+    #Y = matrix[-1]                                       # Use this if Y is in a matrix 
+    identifier, counts = np.unique(np.array(Y), return_counts=True) # Counting the occurances of 0 and 1
+    count_sent = dict(zip(identifier, counts))            # Zip the counts into a dictionary
+    find_majority = max(count_sent, key=count_sent.get)   # Find the majority count from the dict
     print(find_majority)
-    return find_majority
+    return 
