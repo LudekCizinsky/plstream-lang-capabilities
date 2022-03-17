f5bd72579fa60644aad29370ff90ce3985fa3c07
jonas-mika <accounts@jonassenghaas.de>
2022-03-17 11:38:55 +0100

    added loader of raw data

--- src/main.py
+++ src/main.py
@@ -3,7 +3,9 @@ from scripts import load_data
 from scripts.baseline import MajorityClass, LogisticRegression, SOTA
 
 def main():
-  load_data()
+  train_data = load_data(split='train')
+  dev_data = load_data(split='dev')
+  test_data = load_data(split='test')
 
 if __name__ == "__main__":
   main()
--- src/scripts/loader.py
+++ src/scripts/loader.py
@@ -1,4 +1,22 @@
 # loader.py
+import os
+import sys
+import gzip
+import json
 
-def load_data():
-  print('loading')
+def load_data(split='dev'):
+  """
+  Loading specified raw data split as a list of dictionaries.
+  Each dictionary represents a single review.
+  """
+  if split not in ['train', 'dev', 'test']:
+    raise ValueError("split not defined. choose from ['train', 'dev', 'test']")
+  if split == 'test':
+    split = 'test_masked'
+
+  data = []
+  for line in gzip.open(f'data/classification/music_reviews_{split}.json.gz'):
+    review_data = json.loads(line)
+    data.append(review_data)
+
+  return data
