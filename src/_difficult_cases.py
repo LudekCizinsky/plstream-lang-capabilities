10a6c2cec5f4d5b88a46f7b51435c53deeb3f872
Lukas Rasocha <rasocha.lukas@gmail.com>
2022-03-29 17:08:29 +0200

    Added trash function to find negation example - will be removed

--- src/_difficult_cases.py
+++ src/_difficult_cases.py
@@ -10,6 +10,7 @@ import os
 import json
 import spacy
 import checklist
+import argparse
 import numpy as np
 import warnings
 from checklist.editor import Editor
@@ -25,32 +26,21 @@ warnings.filterwarnings("ignore")
 def mft1(n):
   editor = Editor()
   reviews = editor.template(
-    'This {musicterm} {mask} is really not {pos_adj}',
-    musicterm=['album', 'artist', 'producer', 'song', 'tune', 'work'],
-    pos_adj=['good', 'nice', 'enjoyable', 'likeable', 'great'],
-    remove_duplicates=True,
-    nsamples=n).data
+  'This {musicterm} {mask} is really not {pos_adj}',
+  musicterm=['album', 'artist', 'producer', 'song', 'tune', 'work'],
+  pos_adj=['good', 'nice', 'enjoyable', 'likeable', 'great'],
+  remove_duplicates=True,
+  nsamples=n).data
 
   return reviews, ['negative']*n
 
 def mft2(n):
   editor = Editor()
   reviews = editor.template(
-    'I had high expectations for this {musicterm}. But, it ended up being {mask}.',
-    musicterm=['album', 'artist', 'producer', 'song', 'tune', 'work'],
-    remove_duplicates=True,
-    nsamples=n).data
-
-  return reviews, ['negative']*n
-
-def language_test(n):
-  editor = Editor(language='german')
-  reviews = editor.template(
-    'This {musicterm} {mask} is really not {pos_adj}',
-    musicterm=['album', 'artist', 'producer', 'song', 'tune', 'work'],
-    pos_adj=['good', 'nice', 'enjoyable', 'likeable', 'great'],
-    remove_duplicates=True,
-    nsamples=n).data
+  'I had high expectations for this {musicterm}. But, it ended up being {mask}.',
+  musicterm=['album', 'artist', 'producer', 'song', 'tune', 'work'],
+  remove_duplicates=True,
+  nsamples=n).data
 
   return reviews, ['negative']*n
 
@@ -74,48 +64,62 @@ def typo_test(n):
 
 def negation_test(n):
   reviews, sentiments = get_data(stage='extracted', split='train')
+  nlp  = spacy.load('en_core_web_sm')
+  negatives = []
+  reverse_sentiments = []
+  num = 0
+  for review,sentiment in zip(reviews,sentiments):
+    try:
+      if len(review) < 100: #and not re.search(r'[Ss]tars*', review):
+        psentence = list(nlp.pipe([review]))
+        ret = Perturb.perturb(psentence, Perturb.remove_negation)['data'][0][1]
+        while True: 
+          print(review)
+          print(ret)
+          print()
+          ans = input('Correct negation? [y/n] ')
+          if ans == 'y':
+            negatives.append(ret)
+            reverse_sentiments.append('positive' if sentiment == 'negative' else 'negative')
+            num += 1
+            break
+          elif ans == 'n':
+            break
 
-  # load spacy
-  nlp = spacy.load('en_core_web_sm')
-  
-  i = 0
-  negated = []
-  sentiment = []
-  while i<n:
-    idx = np.random.randint(len(reviews))
-    review = reviews[idx]
+          elif ans == 'exit':
+            return
+        
+        if num > n:
+          break
+    except:
+      pass
 
-    negation = Perturb.remove_negation(nlp(review)) 
-    print(negation)
-
-    if negation != review:
-      negated.append(negation)
-      sentiment.append(sentiments[idx])
-      i += 1
-
-  return negated, sentiment
+  return negatives, reverse_sentiments
 
 def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-L','--load', action='store_true', help='load difficult cases otherwise, generate them')
+  args = parser.parse_args()  
+  LOAD = args.load
+
   total = timer()
-  if False:
+  if not LOAD:
     start = working_on('Generating Difficult Cases')
 
     # generate difficult cases using checklist library
-    X_mft1, y_mft1 = mft1(200)
-    X_mft2, y_mft2 = mft2(200)
-    X_typo, y_typo = typo_test(200)
-    # X_neg, y_neg = negation_test(200)
-    X_lang, y_lang = language_test(200)
+    X_mft1, y_mft1 = mft1(25)
+    X_mft2, y_mft2 = mft2(25)
+    X_typo, y_typo = typo_test(25)
+    X_neg, y_neg = negation_test(25)
 
     finished('Generating Difficult Cases', timer() - start)
 
     TESTS = {
-        'mft1': (X_mft1, y_mft1),
-        'mft2': (X_mft2, y_mft2),
-        'typos': (X_typo, y_typo),
-        # 'negation': (X_neg, y_neg),
-        'language': (X_lang, y_lang),
-        }
+      'mft1': (X_mft1, y_mft1),
+      'mft2': (X_mft2, y_mft2),
+      'typos': (X_typo, y_typo),
+       'negation': (X_neg, y_neg),
+      }
     
     difficult_cases = []
     for test_type, data in TESTS.items():
@@ -139,11 +143,11 @@ def main():
     # data/difficult_cases
     finished('Save Difficult Cases', timer() - start)
 
-
-  difficult_cases = []
-  with open('data/difficult_cases/difficult_cases.json', 'r') as f:
-    for line in f:
-      difficult_cases.append(json.loads(line))
+  else:
+    difficult_cases = []
+    with open('data/difficult_cases/difficult_cases.json', 'r') as f:
+      for line in f:
+        difficult_cases.append(json.loads(line))
   
   # load the model and the difficult cases, predict them and
   # report on relevant statistics (should be as low as
