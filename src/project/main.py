68f99ab67a3166ffd45a6453a9cb7c07d3c04610
jonas-mika <accounts@jonassenghaas.de>
2022-05-05 12:43:55 +0200

    reproducing checklist results, mergining plstream training data with checklist tests

--- src/project/main.py
+++ src/project/main.py
@@ -1,70 +1,92 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from scripts.utils import output, working_on, finished
-output('Loading Modules')
-
+# custom modules
 from scripts.models import plstream
-from scripts.utils import get_data, get_encodings
-from scripts.feature_ext import get_training_data, get_test_data
-from scripts.evaluation import evaluate_baseline
-from timeit import default_timer as timer
-import datetime
+from scripts.utils import output, working_on, finished
+
+# default modules
 import os
+import sys
 import json
-PYTHON_PATH = '/Users/co2yd4bcjgh6/.pyenv/shims/python'
+import shutil
+import argparse
+import datetime
+import subprocess
+from timeit import default_timer as timer
+
+# external modules
+import checklist
+from checklist.test_suite import TestSuite
+
+# global envs
+SUITE_PATH = 'sentiment_suite.pkl'
+PRED_PATH = 'predictions'
+PYTHON_PATH = subprocess.check_output(
+    "which python", shell=True).strip().decode('utf-8')
+
+# parser
+parser = argparse.ArgumentParser(
+    description='Control Runflow of main.py')
+
+parser.add_argument(
+    '-R',
+    '--reproduce-checklist',
+    action='store_true',
+      help='Reproduce the Checklist Paper Results')
+
+parser.add_argument(
+    '-F',
+    '--format-plstream-train',
+    action='store_true',
+      help='Format PLStream Training Data and\
+      write to data/final.csv')
+
+args = parser.parse_args()
 
 def main():
   total = timer() # global timer
-  start = working_on('Loading Data')
   
-  # get extracted data (extracted and tokenised)
-  X_train, y_train, X_dev, y_dev = get_data(stage='extracted', split=['train', 'dev']) 
+  # reproduce checklist results
+  if args.reproduce_checklist:
+    s = working_on('Reproduce Checklist Results')
+    suite = TestSuite.from_file(SUITE_PATH)
 
-  # encode labels to integer format
-  word2idx, label2idx = get_encodings(which=['word2idx', 'label2idx'])
-  y_train = [label2idx[label] for label in y_train]
-  y_dev = [label2idx[label] for label in y_dev]
+    models = os.listdir(PRED_PATH)
 
-  # combine training and development set
-  X_train = X_train + X_dev
-  y_train = y_train + y_dev
-  finished('Loading Data', timer() - start)
+    for model in models:
+      sys.stdout = open(f"summaries/{model}.txt", "wt")
 
-  # model training/ evaluation
-  start = working_on('Training and Evaluating Baseline Models')
-  models = evaluate_baseline({"X_train": X_train, "y_train": y_train})
-  finished('Training and Evaluating Baseline Models', timer() - start)
+      print(f"Summary from {model.upper()}")
+      suite.run_from_file(f"{PRED_PATH}/{model}", overwrite=True)
+      suite.summary()
 
-  # testing models
-  start = working_on('Evaluating on Test Data')
-  raw = get_data(stage='raw', split='test')
-  X_test, _  = get_data(stage='extracted', split='test')
+    sys.stdout = sys.__stdout__
+    finished('Reproduce Checklist Results', timer() - s)
 
-  # untokenise test
-  X_test = [' '.join(review) for review in X_test]
+  # checklist pl-stream
+  if args.format_plstream_train:
+    s = working_on('Format PLStream Training Data')
 
-  preds = evaluate_baseline(
-      {"X_test": X_test, 'models': models, 'label2idx': label2idx},
-      training=False
-  )
-  finished('Evaluating on Test Data', timer() - start)
+    # preprocess testing data into format of
+    shutil.copyfile("data/train.csv", "data/final.csv")
+
+    with open("data/final.csv", "a") as final:
+      with open("data/checklist-data", "r") as tests:
+        for test in tests:
+          test = test.strip().replace('"', '')
+          t = f'0,"{test}"\n'
+          final.write(t)
+
+    finished('Format PLStream Training Data', timer() - s)
+
+  # train plstream on this data
+  s = working_on('Checklist PLStream')
   
-  # save test predictions
-  start = working_on('Saving Test Predictions in Original Format')
-  for r, y in zip(raw, preds):
-    r["sentiment"] = y    
-  file_name = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M')
-  path = f"data/predictions/{file_name}.yhat"
-  with open(path, "w") as f:
-    tmp = [json.dumps(r) for r in raw]
-    res = "\n".join(tmp) 
-    f.write(res)
-  print(f"Predictions saved to: {path}")
-  finished('Saving Test Predictions in Original Format', timer() - start)
+  plstream(PYTHON_PATH, data_path="data/final.csv")
 
-  working_on('Import plstream')
-  plstream(PYTHON_PATH)
+  finished('Checklist PLStream', timer() - s)
+
   finished('Entire Pipeline', timer() - total)
 
 if __name__ == "__main__":
